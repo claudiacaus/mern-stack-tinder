be9deb7575214cea93cd7631e20a85c190db971e
Claudia <caucrah@gmail.com>
2021-11-02 01:53:36 +0100

    First setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Header } from './components/Header';
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css';
+
+import PersonIcon from '@material-ui/icons/Person';
+import IconButton from '@material-ui/core/IconButton';
+
+export const Header = () => {
+  return (
+    <div className="header">
+      <IconButton>
+        <PersonIcon fontSize="large" className="header__icon" />
+        <PersonIcon />
+      </IconButton>
+    </div>
+  );
+};
